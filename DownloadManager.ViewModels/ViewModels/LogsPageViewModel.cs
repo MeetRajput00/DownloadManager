f372ebd6111dccdca3becd31ee4834ee67c9c740
Meet Rajput <rajputmeet10@gmail.com>
2024-09-08 22:40:33 +0530

    pages setup done

--- DownloadManager.ViewModels/ViewModels/LogsPageViewModel.cs
+++ DownloadManager.ViewModels/ViewModels/LogsPageViewModel.cs
@@ -4,5 +4,32 @@ namespace DownloadManager.ViewModels
 {
     public class LogsPageViewModel : ViewModelBase
     {
+        private string _logs;
+
+        public string Logs
+        {
+            get { return _logs; }
+            set { _logs = value; RaisePropertyChanged(); }
+        }
+
+        public LogsPageViewModel()
+        {
+            LoadLogs();
+        }
+
+        private void LoadLogs()
+        {
+            string cacheDir = FileSystem.CacheDirectory;
+            var logFiles = Directory.GetFiles(cacheDir, "logs_*.log");
+            string allLogs = string.Empty;
+
+            foreach (var logFile in logFiles)
+            {
+                string logContent = File.ReadAllText(logFile);
+                allLogs += $"Log File: {Path.GetFileName(logFile)}\n{logContent}\n\n";
+            }
+
+            Logs = allLogs;
+        }
     }
 }
