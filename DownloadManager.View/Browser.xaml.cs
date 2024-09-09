15746489e2a1081cd77d9dba8da01cf07a3a4034
Meet Rajput <rajputmeet10@gmail.com>
2024-09-10 00:46:57 +0530

    added action buttons for browser

--- DownloadManager.View/Browser.xaml.cs
+++ DownloadManager.View/Browser.xaml.cs
@@ -1,4 +1,6 @@
 using DownloadManager.ViewModels;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace DownloadManager.View;
 
@@ -11,6 +13,55 @@ public partial class Browser : ContentPage
         InitializeComponent();
         ViewModel = viewModel;
         BindingContext = ViewModel;
+    }
+
+    private void Back_To_Previous_Button_Clicked(object sender, EventArgs e)
+    {
+        if (WebView.CanGoBack)
+        {
+            WebView.GoBack();
+        }
+    }
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        GetDownloadableLinks();
+    }
+
+    private async void GetDownloadableLinks()
+    {
+        var jsCode = @"
+        (function() {
+            var links = document.querySelectorAll('a[href]');
+            var downloadLinks = [];
+            links.forEach(function(link) {
+                var href = link.getAttribute('href');
+                if (href.endsWith('.pdf') || href.endsWith('.zip') || href.endsWith('.jpg')) {
+                    downloadLinks.push(href);
+                }
+            });
+            return JSON.stringify(downloadLinks);
+        })();
+    ";
+
+        var result = await WebView.EvaluateJavaScriptAsync(jsCode);
+
+        // Check if result is valid before attempting to deserialize
+        if (!string.IsNullOrEmpty(result))
+        {
+            try
+            {
+                ViewModel.SetDownloadableLinks(ParseJsonArray(result));
+            }
+            catch (JsonException ex)
+            {
+                Console.WriteLine($"Error parsing JSON: {ex.Message}");
+            }
+        }
+        else
+        {
+            Console.WriteLine("No result returned from JavaScript.");
+        }
     }
 
     private void Go_To_Url_Button_Clicked(object sender, EventArgs e)
@@ -28,6 +79,14 @@ public partial class Browser : ContentPage
         }
     }
 
+    private void Move_Forward_Button_Clicked(object sender, EventArgs e)
+    {
+        if (WebView.CanGoForward)
+        {
+            WebView.GoForward();
+        }
+    }
+
     private void OnWebViewNavigated(object sender, WebNavigatedEventArgs e)
     {
         ViewModel.IsLoading = false;
@@ -36,5 +95,36 @@ public partial class Browser : ContentPage
     private void OnWebViewNavigating(object sender, WebNavigatingEventArgs e)
     {
         ViewModel.IsLoading = true;
+    }
+
+    private List<string> ParseJsonArray(string input)
+    {
+        var results = new List<string>();
+        var currentUrl = new StringBuilder();
+        bool insideString = false;
+
+        foreach (char c in input)
+        {
+            if (c == '\"')
+            {
+                if (insideString)
+                {
+                    results.Add(currentUrl.ToString());
+                    currentUrl.Clear();
+                }
+                insideString = !insideString;
+            }
+            else if (insideString)
+            {
+                currentUrl.Append(c);
+            }
+        }
+
+        return results;
+    }
+
+    private void Reload_Button_Clicked(object sender, EventArgs e)
+    {
+        WebView.Reload();
     }
 }
